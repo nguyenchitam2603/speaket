f5413fe339c221bfb776f965abe94ce54a56870e
tamnguyen <tam.nguyen@virtium.com>
2017-09-09 09:36:23 +0700

    Fix menu not display

--- client/src/components/navigation/sidebar.component.tsx
+++ client/src/components/navigation/sidebar.component.tsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 
 import { FooterComponent } from './footer.component';
 import { MenuComponent } from './menu.component';
-// import { menuController } from './../controllers';
 import * as Routes from './../app.routes';
 
 namespace SidebarComponent {
@@ -20,8 +19,8 @@ export class SidebarComponent extends React.Component<SidebarComponent.Props, Si
       <div className='left_col scroll-view'>
         <div className='navbar nav_title'>
           <Link to={Routes.baseUrl} className='site_title'>
-            <i className='fa fa-paw' />
-            <span>Gentelella Alela!</span>
+            <i className='fa fa-university' />
+            <span> Speaket</span>
           </Link>
         </div>
         <div className='clearfix' />
